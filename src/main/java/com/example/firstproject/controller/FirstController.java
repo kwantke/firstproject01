0032d1615412b0303ec23b6fe2a94d79f2597d61
kwantke <kokk7310@gamil.com>
2022-06-06 23:45:26 +0900

    edit page

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,11 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @RequestMapping("/hi")
-    public String niceToMeeting(){
-        System.out.println();
-        int a =1;
-        int b =2;
-        return "index";
+    public String niceToMeeting(Model model){
+
+       model.addAttribute("username","광규2");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNextTime(Model model){
+
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
     }
 }
 
